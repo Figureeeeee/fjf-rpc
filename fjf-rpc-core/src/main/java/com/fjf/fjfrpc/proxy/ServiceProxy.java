f2395a4004f40d3687c84fc55d5b9e209ce5ac7f
makise <1157552798@qq.com>
2026-01-10 18:47:19 +0800

    1. 封装半包粘包处理器 2. 优化客户端调用代码 3. 清除示例代码

--- fjf-rpc-core/src/main/java/com/fjf/fjfrpc/proxy/ServiceProxy.java
+++ fjf-rpc-core/src/main/java/com/fjf/fjfrpc/proxy/ServiceProxy.java
@@ -1,9 +1,6 @@
 package com.fjf.fjfrpc.proxy;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import com.fjf.fjfrpc.RpcApplication;
 import com.fjf.fjfrpc.config.RpcConfig;
 import com.fjf.fjfrpc.constant.RpcConstant;
@@ -15,19 +12,11 @@ import com.fjf.fjfrpc.registry.Registry;
 import com.fjf.fjfrpc.registry.RegistryFactory;
 import com.fjf.fjfrpc.serializer.Serializer;
 import com.fjf.fjfrpc.serializer.SerializerFactory;
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.net.NetClient;
-import io.vertx.core.net.NetSocket;
-import io.vertx.core.net.SocketAddress;
+import com.fjf.fjfrpc.server.tcp.VertxTcpClient;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CountDownLatch;
 
 /**
  * 服务代理（JDK 动态代理）
@@ -55,8 +44,6 @@ public class ServiceProxy implements InvocationHandler {
                 .args(args)
                 .build();
         try {
-            // 序列化
-            byte[] bodyBytes = serializer.serialize(rpcRequest);
             // 从注册中心获取服务提供者请求地址
             RpcConfig rpcConfig = RpcApplication.getRpcConfig();
             Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
@@ -69,55 +56,10 @@ public class ServiceProxy implements InvocationHandler {
             }
             ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
             // 发送 TCP 请求
-            Vertx vertx = Vertx.vertx();
-            NetClient netClient = vertx.createNetClient();
-            CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
-            netClient.connect(selectedServiceMetaInfo.getServicePort(), selectedServiceMetaInfo.getServiceHost(),
-                    result -> {
-                        if (result.succeeded()) {
-                            System.out.println("Connected to TCP server");
-                            io.vertx.core.net.NetSocket socket = result.result();
-                            // 发送数据
-                            // 构造消息
-                            ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
-                            ProtocolMessage.Header header = new ProtocolMessage.Header();
-                            header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
-                            header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
-                            header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
-                            header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
-                            header.setRequestId(IdUtil.getSnowflakeNextId());
-                            protocolMessage.setHeader(header);
-                            protocolMessage.setBody(rpcRequest);
-                            // 编码请求
-                            try {
-                                Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
-                                socket.write(encodeBuffer);
-                            } catch (IOException e) {
-                                throw new RuntimeException("协议消息编码错误");
-                            }
-
-                            // 接收响应
-                            socket.handler(buffer -> {
-                                try {
-                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage = (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
-                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
-                                } catch (IOException e) {
-                                    throw new RuntimeException("协议消息解码错误");
-                                }
-                            });
-                        } else {
-                            System.err.println("Failed to connect to TCP server");
-                        }
-                    });
-
-            RpcResponse rpcResponse = responseFuture.get();
-            // 记得关闭连接
-            netClient.close();
+            RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
             return rpcResponse.getData();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException("调用失败");
         }
-
-        return null;
     }
 }
--- fjf-rpc-core/src/main/java/com/fjf/fjfrpc/server/tcp/VertxTcpServer.java
+++ fjf-rpc-core/src/main/java/com/fjf/fjfrpc/server/tcp/VertxTcpServer.java
@@ -1,21 +1,12 @@
 package com.fjf.fjfrpc.server.tcp;
 
 import com.fjf.fjfrpc.server.HttpServer;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
-import io.vertx.core.parsetools.RecordParser;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class VertxTcpServer implements HttpServer {
-
-    private byte[] handleRequest(byte[] requestData) {
-        // 在这里编写处理请求的逻辑，根据 requestData 构造响应数据并返回
-        // 这里只是一个示例，实际逻辑需要根据具体的业务需求来实现
-        return "Hello, client!".getBytes();
-    }
 
     @Override
     public void doStart(int port) {
@@ -25,97 +16,8 @@ public class VertxTcpServer implements HttpServer {
         // 创建 TCP 服务器
         NetServer server = vertx.createNetServer();
 
-        // 处理请求 示例代码：测试tcp连接通不通
-//        server.connectHandler(socket -> {
-//            // 处理连接
-//            socket.handler(buffer -> {
-//                // 处理接收到的字节数组
-//                byte[] requestData = buffer.getBytes();
-//                // 在这里进行自定义的字节数组处理逻辑，比如解析请求、调用服务、构造响应等
-//                byte[] responseData = handleRequest(requestData);
-//                // 发送响应
-//                socket.write(Buffer.buffer(responseData));
-//            });
-//        });
-
-//        // 演示粘包问题
-//        server.connectHandler(socket -> {
-//            socket.handler(buffer -> {
-//                String testMessage = "Hello, server!Hello, server!Hello, server!Hello, server!";
-//                int messageLength = testMessage.getBytes().length;
-//                if (buffer.getBytes().length < messageLength) {
-//                    System.out.println("半包, length = " + buffer.getBytes().length);
-//                    return;
-//                }
-//                if (buffer.getBytes().length > messageLength) {
-//                    System.out.println("粘包, length = " + buffer.getBytes().length);
-//                    return;
-//                }
-//                String str = new String(buffer.getBytes(0, messageLength));
-//                System.out.println(str);
-//                if (testMessage.equals(str)) {
-//                    System.out.println("good");
-//                }
-//            });
-//        });
-
-//        // 测试用RecordParser解析固定长度的包，解决半包粘包问题
-//        server.connectHandler(socket -> {
-//            String testMessage = "Hello, server!Hello, server!Hello, server!Hello, server!";
-//            int messageLength = testMessage.getBytes().length;
-//
-//            // 构造parser
-//            RecordParser parser = RecordParser.newFixed(messageLength);
-//            parser.setOutput(new Handler<Buffer>() {
-//
-//                @Override
-//                public void handle(Buffer buffer) {
-//                    String str = new String(buffer.getBytes());
-//                    System.out.println(str);
-//                    if (testMessage.equals(str)) {
-//                        System.out.println("good");
-//                    }
-//                }
-//            });
-//
-//            socket.handler(parser);
-//        });
-
         // 处理请求
-        server.connectHandler(socket -> {
-            // 构造 parser
-            RecordParser parser = RecordParser.newFixed(8);
-            parser.setOutput(new Handler<Buffer>() {
-                // 初始化
-                int size = -1;
-                // 一次完整的读取（头 + 体）
-                Buffer resultBuffer = Buffer.buffer();
-
-                @Override
-                public void handle(Buffer buffer) {
-                    if (-1 == size) {
-                        // 读取消息体长度
-                        size = buffer.getInt(4);
-                        parser.fixedSizeMode(size);
-                        // 写入头信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                    } else {
-                        // 写入体信息到结果
-                        resultBuffer.appendBuffer(buffer);
-                        System.out.println(resultBuffer.toString());
-                        // 重置一轮
-                        parser.fixedSizeMode(8);
-                        size = -1;
-                        resultBuffer = Buffer.buffer();
-                    }
-                }
-            });
-
-            socket.handler(parser);
-        });
-
-        // 处理请求
-//        server.connectHandler(new TcpServerHandler());
+        server.connectHandler(new TcpServerHandler());
 
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
